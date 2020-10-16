faeb0d25f808060534bb2a285ec8888cec2aae1a
sergeliatko <serge@techspokes.com>
2020-10-16 12:13:05 +0200

    Added Pin.php

--- src/Pin.php
+++ src/Pin.php
@@ -10,7 +10,61 @@ namespace SergeLiatko\WPGmaps;
  */
 class Pin {
 
+	/**
+	 * @var \SergeLiatko\WPGmaps\Marker $marker
+	 */
 	protected $marker;
+
+	/**
+	 * @var string $window
+	 */
 	protected $window;
+
+	/**
+	 * Pin constructor.
+	 *
+	 * @param \SergeLiatko\WPGmaps\Marker $marker
+	 * @param string                      $window
+	 */
+	public function __construct( Marker $marker, string $window = '' ) {
+		$this->setMarker( $marker );
+		$this->setWindow( $window );
+	}
+
+	/**
+	 * @return \SergeLiatko\WPGmaps\Marker
+	 */
+	public function getMarker(): Marker {
+		return $this->marker;
+	}
+
+	/**
+	 * @param \SergeLiatko\WPGmaps\Marker $marker
+	 *
+	 * @return Pin
+	 */
+	public function setMarker( Marker $marker ): Pin {
+		$this->marker = $marker;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getWindow(): string {
+		return $this->window;
+	}
+
+	/**
+	 * @param string $window
+	 *
+	 * @return Pin
+	 */
+	public function setWindow( string $window ): Pin {
+		$this->window = $window;
+
+		return $this;
+	}
 
 }
